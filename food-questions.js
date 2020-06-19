8790fc3211430437e3ae44808024b25c10f7f22e
austinjkeener <austingraphicandweb@gmail.com>
2020-06-19 14:07:54 -0400

    getting selections from user choice on home page and storing that information to put into ajax call

---
+++ food-questions.js
@@ -0,0 +1,19 @@
+/**
+ * Questions for the food drop down menu
+ */
+
+ // creating an object with question and answer key pairs
+ var foodQuestions = [
+     {
+         Question: "Protein choice",
+         Choices: ["beef","chicken","pork","tofu","eggs","tuna","salmon","shrimp"]
+     },
+     {
+         Question: "Veggie Choice",
+         Choices: ["broccoli","spinach","asparagus","kale","potato","carrot","tomato","zucchini"]
+     },
+     {
+        Question: "Special Dietary Needs",
+        Choices: ["vegitarian","vegan","dairy-free","peanut-free","low-sugar","soy-free","shellfish-free"]
+     }
+    ]
--- javascript.js
+++ javascript.js
@@ -1,56 +1,60 @@
 /**
  * Project one programming script
  */
-
  // Global variables
  // Function definitions
-$(document).ready(function(){
-     
-    
-    // PUT This within getRecipe function    
+$(document).ready(function(){ 
+  
+  
     // food api
-    var queryUrl = 'https://api.edamam.com/search?q=' + keyword + '&app_id=3e501918&app_key=c95225452f4fe51bc9df3f954ead7e29&ingredients=' + veggie + '&healthLabels=' + healthLabels; //api goes here
-    var recipeParameters = []
-    var keyword = recipeParameters[0]
-    var veggie = recipeParameters[1]
-    var healthLabels = recipeParameters[2]
-    $.ajax({
+    //below are the variables that are put into the queryURl.
+    var keyword;
+    var veggie;
+    var healthLabels;
+    //below is queryURL so which is concaonated with variables so that when the user selects their choices on the web page they are plugged into the queryurl when the request is processed.
+    var queryUrl = 'https://api.edamam.com/search?q=' + keyword + '&app_id=3e501918&app_key=c95225452f4fe51bc9df3f954ead7e29&ingredients=' + veggie + '&healthLabels=' + healthLabels;
+    function foodQuery(){
+          $.ajax({
       url: queryUrl,
       method: "GET"
     }).then(function(response){
       console.log(response);
     })
+    }
 
+// when the function is called it stores the answers to the questions with drop down menu's into an array
+//this function goes into the event listener
+function foodQuestions(){
+  keyword = $("#").val();
+  veggie = $("#").val();
+  healthLabels = $("#").val();
+  foodQuery();
+}
 
-     // movie api
-     var queryURL0 = ''; //api goes here
-     $.ajax({
-       url: queryURL0,
-       method: "GET"
-     }).then(function(response){
-       console.log(response);
-     })
-
-
-
-
-    //Function Definitions
-    //
-
-
-    //EVENT LISTENERS
-    //Create getStarted button event listener
-    $("#get-started").on("click", function(){
-        getStarted();
-    });
+//movie api
+//below is queryURL0 so which is concaonated with variables so that when the user selects their choices on the web page they are plugged into queryurl0 when the request is processed.
+var queryUrl0 = '';
+$.ajax({
+  url: queryUrl0,
+  method: "GET"
+}).then(function(response){
 
 })
 
  // function callers
+ foodQuestions();
  // event listeners
+ $("#get-started").on("click", function(){
+  getStarted();
+});
+
+})
+
+
+
+
+
   // this for loop can be used with a click event so when someone submits their choices they get a randomized result based off of their preferences
-
-
 // var movies = 0 ; //variable that targets the AJAX request  with a list of movies inside of an object array
 // for(i=0;i<movies.length;i++)
 // var random = Math.floor(Math.random().length)
