50136d736fccc7a441b6f017a280881bb9c16032
Christina Starr <christinastarr19@gmail.com>
2020-06-20 15:42:56 -0400

    updated the keyword value in getRecipe function to include protein and veggie in search

--- javascript.js
+++ javascript.js
@@ -4,24 +4,39 @@
 // Global variables
 // Function definitions
 $(document).ready(function () {
-
   // food api
   function getRecipe() {
     $("#dinner-recipe").attr("style", "display: none");
     $("#movie-recs").attr("style", "display:block");
-    var keyword = $("#protein").val();
+    var keywordArray = [];
+    var protein = $("#protein").val();
+    keywordArray.push(protein);
     var veggie = $("#veggie").val();
+    keywordArray.push(veggie);
     var healthLabels = $("#diet").val();
-    var keyword;
+
     var veggie;
     var healthLabels;
-    var queryUrl = 'https://api.edamam.com/search?q=' + keyword + '&app_id=3e501918&app_key=c95225452f4fe51bc9df3f954ead7e29&ingredients=' + veggie + '&healthLabels=' + healthLabels; //api goes here
+    var queryUrl =
+      "https://api.edamam.com/search?q=" +
+      keywordArray +
+      "&app_id=3e501918&app_key=c95225452f4fe51bc9df3f954ead7e29" +
+      "&health=" +
+      healthLabels;
+      console.log(queryUrl);
     $.ajax({
       url: queryUrl,
-      method: "GET"
+      method: "GET",
     }).then(function (response) {
-       console.log(response);
-    })
+      // for (i = 0; i < response.hits; i++) {
+          var randomIndex = Math.floor(Math.random() * 10);  
+        //   console.log(response.hits[randomIndex])
+        // }
+        console.log(randomIndex);
+        console.log(response.hits[randomIndex].recipe.label);
+
+      
+    });
   }
 
   //movie api
@@ -31,50 +46,42 @@ $(document).ready(function () {
 
   // var movieParameters;
 
-
   function getMovie() {
+    $("#movie-recs").attr("style", "display: none");
+    $("#results-view").attr("style", "display:block");
     var movie = $("#movie-input").val().split(" ");
     var genre = $("#genre").val();
-    // need an id for var company below 
+    // need an id for var company below
     // var company = $().val();
     var movie;
     var genre;
     var company;
-    // need to update 
-    var movieQueryUrl = "https://www.omdbapi.com/?t=" + movie + "&apikey=d197feef";
-  
+    // need to update
+    var movieQueryUrl =
+      "https://www.omdbapi.com/?t=" + movie + "&apikey=d197feef";
 
     $.ajax({
       url: movieQueryUrl,
-      method: "GET"
+      method: "GET",
     }).then(function (response) {
       console.log("this function works!");
       console.log(movieQueryUrl);
-
-
-    })
-
+    });
   }
-  
 
   //Function Definitions
-  //function calls 
+  //function calls
   //Created getStarted function
   function getStarted() {
     $("#main-page").attr("style", "display: none");
     $("#dinner-recipe").attr("style", "display:block");
-
-
   }
-
 
   //EVENT LISTENERS
 
   //Create getStarted button event listener
   $("#get-started").on("click", function () {
     getStarted();
-
-
   });
 
   //Create getRecipe button event listener
@@ -88,10 +95,9 @@ $(document).ready(function () {
   });
 });
 
- // function callers
- // event listeners
-  // this for loop can be used with a click event so when someone submits their choices they get a randomized result based off of their preferences
-
+// function callers
+// event listeners
+// this for loop can be used with a click event so when someone submits their choices they get a randomized result based off of their preferences
 
 // this for loop can be used with a click event so when someone submits their choices they get a randomized result based off of their preferences
 // var movies = 0 ; //variable that targets the AJAX request  with a list of movies inside of an object array
