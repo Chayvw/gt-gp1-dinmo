7c0e5d24dd22661acd1c57a2ccbb9913c0a53bcf
Christina Starr <christinastarr19@gmail.com>
2020-06-21 14:43:39 -0400

    updating movie url

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-  
-//**FUNCTION DEFINITIONS */
+  //**FUNCTION DEFINITIONS */
 
   function getRecipe() {
     // Hide dinner recipe container and show movie recommendations container
@@ -15,19 +14,20 @@ $(document).ready(function () {
     var healthLabels = $("#diet").val();
 
     //Create queryUrl based on user's healthLabels choice
-    if($("#diet").val() === "none"){
-      var queryUrl = "https://api.edamam.com/search?q=" +
-      keywordArray +
-      "&app_id=3e501918&app_key=c95225452f4fe51bc9df3f954ead7e29";
+    if ($("#diet").val() === "none") {
+      var queryUrl =
+        "https://api.edamam.com/search?q=" +
+        keywordArray +
+        "&app_id=3e501918&app_key=c95225452f4fe51bc9df3f954ead7e29";
     } else {
       var queryUrl =
-      "https://api.edamam.com/search?q=" +
-      keywordArray +
-      "&app_id=3e501918&app_key=c95225452f4fe51bc9df3f954ead7e29" +
-      "&health=" +
-      healthLabels;
+        "https://api.edamam.com/search?q=" +
+        keywordArray +
+        "&app_id=3e501918&app_key=c95225452f4fe51bc9df3f954ead7e29" +
+        "&health=" +
+        healthLabels;
     }
-      //Make ajax call to get response data
+    //Make ajax call to get response data
     $.ajax({
       url: queryUrl,
       method: "GET",
@@ -43,57 +43,72 @@ $(document).ready(function () {
       $("#recipe-title").text(response.hits[randomIndex].recipe.label);
       var imgURL = response.hits[randomIndex].recipe.image;
       $("#recipe-icon").attr("src", imgURL);
-      $("#recipe-source").text("Source: " + response.hits[randomIndex].recipe.source);
+      $("#recipe-source").text(
+        "Source: " + response.hits[randomIndex].recipe.source
+      );
       var recipeURL = response.hits[randomIndex].recipe.url;
-      $("#recipe-url").attr("href" , recipeURL);
+      $("#recipe-url").attr("href", recipeURL);
       $("#recipe-url-text").text("Click Here for Recipe");
 
       //Add text, image and url to Results for recipe2
       $("#recipe-title2").text(response.hits[randomIndex2].recipe.label);
       var imgURL = response.hits[randomIndex2].recipe.image;
       $("#recipe-icon2").attr("src", imgURL);
-      $("#recipe-source2").text("Source: " + response.hits[randomIndex2].recipe.source);
+      $("#recipe-source2").text(
+        "Source: " + response.hits[randomIndex2].recipe.source
+      );
       var recipeURL = response.hits[randomIndex2].recipe.url;
-      $("#recipe-url2").attr("href" , recipeURL);
+      $("#recipe-url2").attr("href", recipeURL);
       $("#recipe-url-text-2").text("Click Here for Recipe");
 
       //Add text, image and url to Results for recipe3
       $("#recipe-title3").text(response.hits[randomIndex3].recipe.label);
       var imgURL = response.hits[randomIndex3].recipe.image;
       $("#recipe-icon3").attr("src", imgURL);
-      $("#recipe-source3").text("Source: " + response.hits[randomIndex3].recipe.source);
+      $("#recipe-source3").text(
+        "Source: " + response.hits[randomIndex3].recipe.source
+      );
       var recipeURL = response.hits[randomIndex3].recipe.url;
-      $("#recipe-url3").attr("href" , recipeURL);
+      $("#recipe-url3").attr("href", recipeURL);
       $("#recipe-url-text-3").text("Click Here for Recipe");
     });
   }
-
 
   function getMovie() {
     $("#movie-recs").attr("style", "display: none");
     $("#results-view").attr("style", "display:block");
     var movie = $("#movie-input").val().split(" ");
-    var genre = $("#genre").val();
+    var genre = $("#genre option:selected").val();
+    var company = $("#company option:selected").val();
     // need an id for var company below
     // var company = $().val();
     var movie;
     var genre;
-    var company;
-    // need to update
-    var movieQueryUrl =
-      "https://www.omdbapi.com/?t=" + movie + "&apikey=d197feef";
+    console.log(genre);
 
-    $.ajax({
-      url: movieQueryUrl,
-      method: "GET",
-    }).then(function (response) {
-      console.log("this function works!");
-      console.log(movieQueryUrl);
-    });
+    // This URL needs to be tested and we need to find the movie ids.
+    var movieQueryUrl =
+      "https://api.themoviedb.org/3/movie/" +
+      movie +
+      "/similar?api_key=aced25e61f7d1bd5c92fcbfed28dc07c&language=en-US" +
+      "&with_genres" +
+      genre;
+    +(
+      // "&with_companies=" +
+      // company; +
+      // "with_keywords" +
+      // movie;
+
+      $.ajax({
+        url: movieQueryUrl,
+        method: "GET",
+      }).then(function (response) {
+        console.log("this function works!");
+        console.log(response);
+      })
+    );
   }
 
-  
-  
   //Created getStarted function
   function getStarted() {
     $("#main-page").attr("style", "display: none");
@@ -119,8 +134,6 @@ $(document).ready(function () {
 
   //Create startOver button event listener
   $("#resetBtn").on("click", function () {
-    window.location.reload();;
+    window.location.reload();
   });
-
 });
-
