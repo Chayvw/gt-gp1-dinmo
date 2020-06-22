408cbda31a7097a758b844e73900dc6e34f996ef
Christina Starr <christinastarr19@gmail.com>
2020-06-22 14:30:12 -0400

    added placeholder for movie input

--- javascript.js
+++ javascript.js
@@ -76,60 +76,64 @@ $(document).ready(function () {
   function getMovie() {
     $("#movie-recs").attr("style", "display: none");
     $("#results-view").attr("style", "display:block");
-    var movie = $("#movie-input").val().split(" ");
+    var movie = $("#movie-input").val();
     var genre = $("#genre option:selected").val();
     var company = $("#company option:selected").val();
-    // need an id for var company below
-    // var company = $().val();
-    var movie;
-    var genre;
-
-    // This URL needs to be tested and we need to find the movie ids.
-    var movieQueryUrl =
-      "https://api.themoviedb.org/3/movie/" +
-      movie +
-      "/similar?api_key=aced25e61f7d1bd5c92fcbfed28dc07c&language=en-US" +
-      "&with_genres" +
-      genre +
-      "&with_companies=" +
-      company +
-      "with_keywords" +
-      movie;
-
+   
+    var movieSearch = "https://api.themoviedb.org/3/search/movie?api_key=aced25e61f7d1bd5c92fcbfed28dc07c&language=en-US&page=1&include_adult=false&query=" + movie;
     $.ajax({
-      url: movieQueryUrl,
+      url: movieSearch,
       method: "GET",
     }).then(function (response) {
-      var movieIndex = Math.floor(Math.random() * 10);
-      var movieIndex2 = Math.floor(Math.random() * 10);
-      var movieIndex3 = Math.floor(Math.random() * 10);
-      
-      //Add text, image and url to Results for movie1 use movieIndex
-      $("#movie-title").text(response.results[movieIndex].title);
-      var movieImgURL = response.results[movieIndex].poster_path;
-      $("#movie-icon").attr("src", JSON.stringify(movieImgURL));
-      $("#movie-year").text("Year: " + response.);
-      var movieRentURL = fillInURLPathHere;
-      $("#movie-url").attr("href", movieRentURL);
-      $("#movie-url-text").text("Click To Rent This Movie");
+      console.log("movieSearch", response.results[0].id)
+      var movieID = response.results[0].id;
+      // This URL needs to be tested and we need to find the movie ids.
+      var movieQueryUrl =
+        "https://api.themoviedb.org/3/movie/" +
+        movieID +
+        "/similar?api_key=aced25e61f7d1bd5c92fcbfed28dc07c&language=en-US" +
+        "&with_genres=" +
+        genre +
+        "&with_companies=" +
+        company +
+        "&with_keywords=" +
+        movie;
 
-      //Add text, image and url to Results for recipe2 use movieIndex2
-      $("#movie-title2").text(fillInTitle2PathHere);
-      var movieImgURL = fillInPathHere;
-      $("#movie-icon2").attr("src", movieImgURL);
-      $("#movie-year2").text("Year: " + fillInPathHere);
-      var movieRentURL = fillInPathHere;
-      $("#movie-url2").attr("href", movieRentURL);
-      $("#movie-url-text-2").text("Click To Rent This Movie");
+      $.ajax({
+        url: movieQueryUrl,
+        method: "GET",
+      }).then(function (response2) {
+        var movieIndex = Math.floor(Math.random() * 20);
+        var movieIndex2 = Math.floor(Math.random() * 20);
+        var movieIndex3 = Math.floor(Math.random() * 20);
+        console.log(response2);
+        console.log(movieIndex);
+        console.log(movieIndex2);
+        console.log(movieIndex3);
 
-      //Add text, image and url to Results for recipe3, use movieIndex3
-      $("#movie-title3").text(fillInTitle3PathHere);
-      var movieImgURL = fillInPathHere;
-      $("#movie-icon3").attr("src", movieImgURL);
-      $("#movie-year3").text("Year: " + fillInPathHere);
-      var movieRentURL = fillInPathHere;
-      $("#movie-url3").attr("href", movieRentURL);
-      $("#movie-url-text-3").text("Click To Rent This Movie");
+        //Add text, image and url to Results for movie1 use movieIndex
+        $("#movie-title").text(response2.results[movieIndex].title);
+        var movieImgURL = "https://image.tmdb.org/t/p/original" + response2.results[movieIndex].poster_path;
+        console.log(response2.results[movieIndex].poster_path)
+        $("#movie-icon").attr("src", movieImgURL);
+        $("#movie-year").text("Year: " + response2.results[movieIndex].release_date);
+        console.log(response2.results[movieIndex].release_date);
+        $("#movie-overview").text("Summary: " + response2.results[movieIndex].overview);
+
+        //Add text, image and url to Results for recipe2 use movieIndex2
+        $("#movie-title2").text(response2.results[movieIndex2].title);
+        var movieImgURL2 = "https://image.tmdb.org/t/p/original" + response2.results[movieIndex2].poster_path;
+        $("#movie-icon2").attr("src", movieImgURL2);
+        $("#movie-year2").text("Year: " + response2.results[movieIndex2].release_date);
+        $("#movie-overview-2").text("Summary: " + response2.results[movieIndex2].overview);
+
+        //Add text, image and url to Results for recipe3, use movieIndex3
+        $("#movie-title3").text(response2.results[movieIndex3].title);
+        var movieImgURL3 = "https://image.tmdb.org/t/p/original" + response2.results[movieIndex3].poster_path;
+        $("#movie-icon3").attr("src", movieImgURL3);
+        $("#movie-year3").text("Year: " + response2.results[movieIndex3].release_date);
+        $("#movie-overview-3").text("Summary: " + response2.results[movieIndex3].overview);
+      });
     });
   }
 
